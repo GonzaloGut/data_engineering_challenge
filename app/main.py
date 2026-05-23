eafdd9be453b4772403c72f0177fdce1e7207b5a
Gonzalo <gonzaloglr23@gmail.com>
2026-05-23 17:08:48 -0500

    Update main, tables added

--- app/main.py
+++ app/main.py
@@ -1,9 +1,15 @@
 from fastapi import FastAPI
+from app.database.database import Base, engine
+from app.models.department import Department
+from app.models.job import Job
+from app.models.hired_employee import HiredEmployee
 
 app = FastAPI(
     title="Data Engineering Challenge API",
     version="1.0.0"
 )
+
+Base.metadata.create_all(bind=engine)
 
 @app.get("/")
 def healtcheck():
