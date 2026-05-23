632ddd994a9d20cdac3dfe454409d976358f581d
Gonzalo <gonzaloglr23@gmail.com>
2026-05-23 17:19:54 -0500

    Update hired_employee, index added

--- app/models/hired_employee.py
+++ app/models/hired_employee.py
@@ -3,20 +3,22 @@ from app.database.database import Base
 
 class HiredEmployee(Base):
     __tablename__ = "hired_employees"
-    
+
     id = Column(Integer, primary_key=True, index=True)
     name = Column(String, nullable=False)
-    datetime = Column(DateTime, nullable=False)
+    datetime = Column(DateTime, nullable=False, index=True)
 
     department_id = Column(
         Integer, 
         ForeignKey("departments.id"),
-        nullable=False
+        nullable=False,
+        index=True
     )
 
     job_id = Column(
         Integer,
         ForeignKey("jobs.id"),
-        nullable=False
+        nullable=False,
+        index=True
     )
 
